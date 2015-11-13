f6eb5a8853ef38d5c001f96084362d917973484e
evely211 <569197531@qq.com>
2015-11-13 21:49:16 +0800

    Add FileDestory

--- C/C_Algorithm/fileDestroy/fileDestoryer.c
+++ C/C_Algorithm/fileDestroy/fileDestoryer.c
@@ -5,3 +5,30 @@
 *
 *
 */
+
+#include "fileDestoryer.h"
+
+void notice(int i,char *s) {
+	if(i == 1) {
+		printf("\n");
+	} else {
+		fprintf(stderr,"Usage:%s [filename1] <filename2> ...\n",s);
+	}
+}
+
+
+int main(int argc,char *argv[]) {
+	int i,j;
+	char ch;
+
+	FILE *f;
+
+	notice(1,argv[0]);
+
+	if(argc <2) {
+		notice(2,argv[0]);
+		exit(0);
+	}
+
+	return 0;
+}
---
+++ C/C_Algorithm/fileDestroy/fileDestoryer.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void notice(int i,char *s);
+
+void wipe(FILE *f, char c);
+
+long file_size(FILE *f);
+
+int require(int c,char *s[]);
