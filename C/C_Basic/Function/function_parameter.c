329f9cea612d72a196d98275fdccb769c9770c42
evely211 <569197531@qq.com>
2015-11-17 15:48:58 +0800

    Modify function parameter

--- C/C_Basic/Function/function_parameter.c
+++ C/C_Basic/Function/function_parameter.c
@@ -19,9 +19,41 @@ void get_max_min(int a,int b,int* get_max,int *get_min) {
 	*get_min = a<b?a:b;
 }
 
+void swap_mn(int *m,int *n,int len) {
+	int tmp;
+	int i=0;
+	while(i<len) {
+		tmp=*m;
+		*m = *n;
+		*n =tmp;
+		*m++;
+		*n++;
+		i++;
+	}
+}
+
+void printfmn(int *m,int *n,int len) {
+	int i;
+	printf("m=");
+	for(i=0;i<len;i++) {
+		printf("%d",*m++);
+	}
+	printf("\n");
+
+	printf("n=");
+	for(i=0;i<len;i++) {
+		printf("%d",*n++);
+	}
+	printf("\n");
+}
+
+
 int main() {
 	int a = 10,b = 20;
 	int get_max,get_min;
+	int m[] = {1,3,5,7,9};
+	int n[] = {2,4,6,8,10};
+	int len =5;
 
 	printf("sum=%d\n",add(a,b));
 
@@ -32,6 +64,16 @@ int main() {
 	get_max_min(a,b,&get_max,&get_min);
 
 	printf("max=%dmin=%d\n",get_max,get_min);
+
+	printf("m=%p\tn=%p\n",&m,&n);
+
+	printf("Before Exchange:\n");
+	printfmn(m,n,len);
+
+	swap_mn(m,n,len);
+	
+	printf("After Exchange:\n");
+	printfmn(m,n,len);
 
 	return 0;
 }
