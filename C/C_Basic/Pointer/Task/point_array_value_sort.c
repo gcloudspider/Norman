1e2bd97ed7cafc41193d8d346e7393f9c68da7b9
evely211 <569197531@qq.com>
2015-11-17 13:31:39 +0800

    Merge

--- C/C_Basic/Pointer/Task/point_array_value_sort.c
+++ C/C_Basic/Pointer/Task/point_array_value_sort.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 int main() {
-	int m[10] = {1,2,5,3,7};
+	int m[6] = {8,2,5,3,7};
 
 	scanf("%d",&m[5]);
 
 	int *pm = m;
-	int i,tmp;
-
-	while(*pm) {
-		if(*pm >*(pm+1)){
-			tmp = *pm;
-			*pm = *(pm+1);
-			*(pm+1) = tmp;
+	int i,j,tmp;
+	
+	for(i=1;i<6;i++) {
+		for(j=0;j<6-i;j++) {
+			if(*(pm+j) >(*(pm+j+1))) {
+				tmp = *(pm+j);
+				*(pm+j) = *(pm+j+1);
+				*(pm+j+1) = tmp;
+			}
 		}
-		*pm ++;
 	}
-
+	
 	for(i=0;i<6;i++) {
 		printf("%d",m[i]);
 	}
---
+++ C/C_Basic/Pointer/Task/point_array_value_zero.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+	it m[5] = {1,0,3,0,5};
+
+	int *pm = m;
+
+	while(*pm) {
+		if
+		*pm++;
+	}
+
+	return 0;
+}
