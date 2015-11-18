1444a909398f23c4a4f45e6a9b02502ce5175cb2
evely211 <569197531@qq.com>
2015-11-18 08:50:13 +0800

    Merge code

--- C/C_Basic/Function/function_strnrev.c
+++ C/C_Basic/Function/function_strnrev.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-char* strnrev(char *s,int n) {
+char* my_strnrev(char *s,int n) {
 	if(s== NULL)
 		return NULL;
 	int i=1,j=0;
 	int tmp;
-	int *pm,pn;
+	char *pm,*pn;
 	char* dest = s;
+	printf("before rev:%s\n",s);
 
-	while(*s != '\0')
+	while(*s != '\0') {
 		*s++;
 		j++;
+	}
 
 	if(n>0)
-		while((i<j) &&(i != n)) {
+		while((i<j)&&(i!=n+1)) {
 			pm = s-i;
 			pn = s-j;
-			
+			tmp = *pn;
+			*pn = *pm;
+			*pm = tmp;
+			j--;
+			i++;
 		}
 
 	return dest;
@@ -24,12 +30,12 @@ char* strnrev(char *s,int n) {
 
 
 int main() {
-	char str[] = "aiscoke";
+	char str[] = "zxcvbnm,.aiscokqwasdfghjklertyuiop[e";
 	
 	int len;
 	scanf("%d",&len);
 
-	printf("%s\n",my_strnrev(str,len));
+	printf("After rev:%s\n",my_strnrev(str,len));
 	
 	return 0;
 }
