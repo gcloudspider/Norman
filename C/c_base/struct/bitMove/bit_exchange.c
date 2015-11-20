9546805403d8dac7473e54612fccc6b983a0dae0
evely211 <569197531@qq.com>
2015-11-20 08:21:20 +0800

    Modify git ignore global setting

--- C/c_base/struct/bitMove/bit_exchange.c
+++ C/c_base/struct/bitMove/bit_exchange.c
@@ -3,16 +3,22 @@
 int main() {
 
 	int a = 0x12345678;
+	int b,c;
 
+	printf("0x%x\n",a);
 	printf("Before exchange a Bin=");
 	decToBin(a,2);
 	printf("\n");
 	
-	int x = 0x56781234;
+	b=(a/16)+(a%16)*16;
+	
+	//c=rcol(a,4);
+	//int x = 0x56781234;
 
-	printf("Before exchange x Bin=");
-	decToBin(x,2);
+	printf("After exchange a Bin=");
+	decToBin(b,2);
 	printf("\n");
 	
+	printf("0x%x\n",b);
 	return 0;
 }
