ec121c0436070fbb35f4217fd7bd2b31222fd7f4
evely211 <569197531@qq.com>
2015-11-23 21:53:43 +0800

    modify single link list

--- c/c_advanced/linkedlist/linklist_singlet.c
+++ c/c_advanced/linkedlist/linklist_singlet.c
@@ -8,6 +8,10 @@
 ##
 ##Analyze: 
 ##
+##      算法分析:
+##          创建表,读入数据,生成新结点
+##          将新结点插入到当前链表的表尾结点之后
+##
 ##Induction:
 ##
 ##Summary:
@@ -15,6 +19,7 @@
 ##########################################################*/
 
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct student {
     int stu_num;
@@ -26,32 +31,34 @@ typedef struct student {
 
 
 int main() {
-    STU* head= NULL,Tail=NULL;
-    STU* pn;
+    STU* head= NULL,*Tail=NULL;
+    STU *pn,*pm;
     char ch;
 
+    pn = malloc(sizeof(STU));
+    if(pn == NULL) {
+        printf("malloc error!\n");
+        return -1;
+    }
+
+    
     while(1) {
         //1.申请内存
         //2.初始化pn
         //head->pn
         //pn->next -> 下一个pn指针
-        pn = malloc(sizeof(STU));
-        if(pn == NULL) {
+        
+        Tail = pn;
+
+        pm = malloc(si zeof(STU));
+        if(pm == NULL) {
             printf("malloc error!\n");
             return -1;
         }
-
-        scanf("%d%s%d%d",
-             &pn->stu_num,
-             pn->stu_name,
-             &pn->stu_age,
-             &pn->stu_score
-             );
-        while((ch=getchar())!= '\n');
-
-        head = pn;
+        Tail->next = pm;
+        Tail=pm;
         
-
+        printf("is continue? please input yes or no(Y/N):\n");
 clear_method:
         ch = getchar();
         if(ch == 'n' || ch=='N') {
@@ -59,14 +66,21 @@ clear_method:
         } else if(ch == '\n'|| ch == 'y' || ch == 'Y') {
             continue;
         } else{
-            goto clear_method
+            goto clear_method;
         }
 
     }
 
-    while(1) {
-        
-
+    pn = head;
+    printf("num\tname\tage\tscore\n");
+    while(pn) {
+        printf("%d\t%s\t%d\t%d\n",
+              pn->stu_num,
+              pn->stu_name,
+              pn->stu_age,
+              pn->stu_score
+              );
+        pn = pn->next;
     }
 
     return 0;
