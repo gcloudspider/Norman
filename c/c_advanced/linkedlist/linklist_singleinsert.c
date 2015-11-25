e58897fb78a0708df9f42a7afc536e834c955fab
evely211 <569197531@qq.com>
2015-11-25 12:46:44 +0800

    modify single link list insert

--- c/c_advanced/linkedlist/linklist_singleinsert.c
+++ c/c_advanced/linkedlist/linklist_singleinsert.c
@@ -20,3 +20,71 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+
+typedef struct student{
+    int stu_num;
+    char stu_name[64];
+    int stu_age;
+    int stu_score;
+    struct student *next;
+}STU;
+
+
+void create_link_list(STU** head) {
+    STU* pn;
+
+    while(1) {
+        pn = malloc(sizeof(STU));
+        if(pn == NULL) {
+            printf("malloc error!\n");
+            return ;
+        }
+
+        scanf("%d%s%d%d",
+             &pn->stu_num,
+             pn->stu_name,
+             &pn->stu_age,
+             &pn->stu_score
+             );
+
+        while((ch=getchar())!='\n');
+        pn->next = *head;
+        head = pn;
+
+        printf("is continue ? please input yes or no(Y/N): ");
+clear_enter:
+        ch=getchar();
+        if(ch=='n'||ch=='N') {
+            break;
+        } else if(ch=='y'|| ch=='Y'||ch=='\n') {
+            continue;
+        } else {
+            goto clear_enter;
+        }   
+    }
+}
+
+void show_link_list(STU** head) {
+    STU* pn;
+    pn = *head;
+    while(pn) {
+        printf("%d%s%d%d",
+               pn->stu_num,
+              pn->stu_name,
+              pn->stu_age,
+              pn->stu_score
+              );
+        pn = pn->next;
+    }
+    return ;
+}
+
+
+int main() {
+    STU* head = NULL;
+    create_link_list(&head);
+
+    show_link_list(&head);
+
+    return 0;
+}
