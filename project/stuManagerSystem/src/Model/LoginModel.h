e13611d229ce013163a9e6f6fe73aa1cc0373e2c
evely211 <569197531@qq.com>
2015-11-28 10:52:32 +0800

    add Login Model

---
+++ project/stuManagerSystem/src/Model/LoginModel.h
@@ -0,0 +1,24 @@
+/*########################################################
+##File Name: LoginModel.h
+##Created Time:2015年11月28日 星期六 09时55分05秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#ifndef _LOGINMODEL_H
+#define _LOGINMODEL_H
+
+int isLoginSuccessful(int username,char passwd[]);
+
+
+
+#endif
