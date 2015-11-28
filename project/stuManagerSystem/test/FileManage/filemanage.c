61448a2172c311a9e68d73129b555926a4bac260
evely211 <569197531@qq.com>
2015-11-28 21:10:46 +0800

    add test write student double link list data to file method

--- project/stuManagerSystem/test/FileManage/filemanage.c
+++ project/stuManagerSystem/test/FileManage/filemanage.c
@@ -18,13 +18,55 @@
 #include "../../src/DataManager/FileManager.h"
 #include "../../src/utils/DataStruct.h"
 
+
+int delete_double_link_list(STU **head,int n) {
+    STU* pn;
+    pn = *head;
+    //1.查找要删除的节点
+    while(pn) {
+        if(pn->id == n) break;
+        pn = pn->next;
+    }
+
+    if(pn){
+        if(pn == *head) {
+            //要删除的是第一个
+            if(pn->next) {          //是否为最后一个
+                pn->next->pre = NULL;
+                *head = pn->next;
+            }else {
+                *head=NULL;          //是最后一个,解除head指针指向
+            }
+        } else {
+            if(pn->next){
+             pn->next->pre=pn->pre;
+             pn->pre->next = pn->next;
+            }else {
+                pn->pre->next = pn->next;
+            }
+        }
+    } else {
+        return -1;
+    }
+    return 0;
+}
+
+
 int main() {
     STU* head_student = NULL;
     TEA* head_teacher = NULL;
+    
     ReadStudentDataToLinkList(&head_student);
     PrintStudentDoubleLinkList(&head_student);
 
     ReadTeacherDataToLinkList(&head_teacher);
     PrintTeacherDoubleLinkList(&head_teacher);
+
+    delete_double_link_list(&head_student,1001);
+    delete_double_link_list(&head_student,1006);
+    PrintStudentDoubleLinkList(&head_student);
+
+    WriteDataToStudentFile(&head_student);
+
     return 0;
 }
