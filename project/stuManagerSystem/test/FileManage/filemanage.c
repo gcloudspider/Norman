e795461bbafcb4bb233e59fe3b55fd06849b255c
evely211 <569197531@qq.com>
2015-11-28 10:55:08 +0800

    add test filemanager

---
+++ project/stuManagerSystem/test/FileManage/filemanage.c
@@ -0,0 +1,28 @@
+/*########################################################
+##File Name: filemanage.c
+##Created Time:2015年11月28日 星期六 10时20分57秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "../../src/DataManager/FileManager.h"
+#include "../../src/utils/DataStruct.h"
+
+int main() {
+    STU* head = NULL;
+    
+    ReadStudentDataToLinkList(&head);
+
+
+    return 0;
+}
