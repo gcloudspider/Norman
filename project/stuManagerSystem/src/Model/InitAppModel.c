107701f32948d6b2defa2534d1c06f48cd5c4884
evely211 <569197531@qq.com>
2015-11-29 11:26:24 +0800

    add init app Model method

---
+++ project/stuManagerSystem/src/Model/InitAppModel.c
@@ -0,0 +1,25 @@
+/*########################################################
+##File Name: InitAppModel.c
+##Created Time:2015年11月29日 星期日 11时17分53秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "InitAppModel.h"
+
+void InitAppModel() {
+    InitStudentModel();
+    InitTeacherModel();
+    InitAdminModel();
+    return ;
+}
---
+++ project/stuManagerSystem/src/Model/InitAppModel.h
@@ -0,0 +1,22 @@
+/*########################################################
+##File Name: InitAppModel.h
+##Created Time:2015年11月29日 星期日 11时17分46秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#ifndef _INITAPPMODEL_H
+#define _INITAPPMODEL_H
+
+void InitAppModel();
+
+#endif
