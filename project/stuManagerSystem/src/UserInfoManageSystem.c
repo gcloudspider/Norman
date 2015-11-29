20056089f0dda47ad62dff76fd71653ee831d6c0
evely211 <569197531@qq.com>
2015-11-29 11:28:11 +0800

    Add Main Program

---
+++ project/stuManagerSystem/src/UserInfoManageSystem.c
@@ -0,0 +1,25 @@
+/*########################################################
+##File Name: UserInfoManageSystem.c
+##Created Time:2015年11月29日 星期日 11时15分57秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "./Model/InitAppModel.h"
+
+int main() {
+    
+    InitModel();
+
+    return 0;
+}
