fd5ff261e72ba96536c16e5cff00cb857147ae1b
evely211 <569197531@qq.com>
2015-11-30 09:11:45 +0800

    add release app model

---
+++ project/stuManagerSystem/src/Model/ReleaseAppModel.c
@@ -0,0 +1,28 @@
+/*########################################################
+##File Name: ReleaseAppModel.c
+##Created Time:2015年11月30日 星期一 08时36分30秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "ReleaseAppModel.h"
+
+void ReleaseAllModel() {
+    //TODO:将链表内容回写文件
+    
+
+
+    //TODO:释放链表
+    printf("release app finished !\n");
+    return ;
+}
---
+++ project/stuManagerSystem/src/Model/ReleaseAppModel.h
@@ -0,0 +1,22 @@
+/*########################################################
+##File Name: ReleaseAppModel.h
+##Created Time:2015年11月30日 星期一 08时35分56秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#ifndef _RELEASEAPPMODEL_H
+#define _RELEASEAPPMODEL_H
+
+void ReleaseAllModel();
+
+#endif
