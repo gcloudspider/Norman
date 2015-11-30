a130a5d9fbf3441ef84afad6978773bf979b0378
evely211 <569197531@qq.com>
2015-11-30 08:21:08 +0800

    add global variable

---
+++ project/stuManagerSystem/src/utils/global.c
@@ -0,0 +1,21 @@
+/*########################################################
+##File Name: global.c
+##Created Time:2015年11月29日 星期日 22时03分38秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "global.h"
+STU* head_student;
+TEA* head_teacher;
+ADMIN* head_admin;
---
+++ project/stuManagerSystem/src/utils/global.h
@@ -0,0 +1,23 @@
+/*########################################################
+##File Name: global.h
+##Created Time:2015年11月29日 星期日 21时53分34秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#ifndef _GLOBAL_H
+#define _GLOBAL_H
+#include "DataStruct.h"
+extern STU* head_student;
+extern TEA* head_teacher;
+extern ADMIN* head_admin;
+#endif
