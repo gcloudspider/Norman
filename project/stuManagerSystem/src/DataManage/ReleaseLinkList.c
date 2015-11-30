6e04121c3dea3ecb76100b584a36ccae7f84fbfb
evely211 <569197531@qq.com>
2015-11-30 10:39:16 +0800

    add data manage release link list

---
+++ project/stuManagerSystem/src/DataManage/ReleaseLinkList.c
@@ -0,0 +1,50 @@
+/*########################################################
+##File Name: ReleaseLinkList.c
+##Created Time:2015年11月30日 星期一 10时02分05秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "ReleaseLinkList.h"
+#include "../utils/DataStruct.h"
+
+int ReleaseStudentLinkList(STU *head) {
+    STU *pn = NULL;
+    while(head) {
+        pn = head->next;
+        free(head);
+        head = pn;
+    }
+    return 0;
+}
+
+int ReleaseTeacherLinkList(TEA *head) {
+    TEA *pn = NULL;
+    while(head) {
+        pn = head->next;
+        free(head);
+        head = pn;
+    }
+    return 0;
+}
+
+int ReleaseAdminLinkList(ADMIN *head) {
+    ADMIN *pn = NULL;
+    while(head) {
+        pn = head->next;
+        free(head);
+        head = pn;
+    }
+    return 0;
+}
---
+++ project/stuManagerSystem/src/DataManage/ReleaseLinkList.h
@@ -0,0 +1,26 @@
+/*########################################################
+##File Name: ReleaseLinkList.h
+##Created Time:2015年11月30日 星期一 10时01分58秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#ifndef _RELEASELINKLIST_H
+#define _RELEASELINKLIST_H
+#include "../utils/DataStruct.h"
+
+int ReleaseStudentLinkList(STU *head);
+int ReleaseTeacherLinkList(TEA *head);
+int ReleaseAdminLinkList(ADMIN *head);
+
+
+#endif
