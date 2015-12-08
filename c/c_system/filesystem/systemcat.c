a5ae812a8309320e0c5751deb9a4fb0a9e021a5c
evely211 <569197531@qq.com>
2015-12-08 15:25:46 +0800

    add system program

---
+++ c/c_system/filesystem/systemcat.c
@@ -0,0 +1,70 @@
+/*########################################################
+##File Name: systemopen.c
+##Created Time:2015年12月08日 星期二 10时28分10秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#define MAXLEN 10
+
+void mycat(int fd){
+    char buffer[MAXLEN] = {0};
+    int ret;
+    int i;
+
+    while(1){
+        ret = read(fd,buffer,MAXLEN);
+        if(ret < MAXLEN){
+            break;
+        }
+        printf("%s",buffer);
+    }
+
+    if(ret >0) {
+        //buffer[ret] = 0;
+        printf("%s",buffer);
+    }
+}
+
+int isHere(const char* filename){
+    int fd;
+    fd = open(filename,O_RDONLY);
+    if(-1 == fd){
+        printf("errno=%d\n",errno);
+        perror("error:");
+        return fd;
+    }
+    //printf("%d\n",fd);    0:stdin  1:stdout  2: stderr
+    //close(fd);
+    return fd;
+}
+
+int main(){
+    int ret;
+    ret = isHere("a.txt");
+
+    if(-1==ret){
+        printf("file not exist!\n");
+    } else {
+        //printf("file exist!\n");
+        mycat(ret);
+        
+        close(ret);
+    }
+    return 0;
+}
---
+++ c/c_system/filesystem/systemcp.c
@@ -0,0 +1,87 @@
+/*########################################################
+##File Name: systemcp.c
+##Created Time:2015年12月08日 星期二 14时02分59秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAXLEN 10
+
+int mycp(const char* dst,const char* src){
+    int fs,fd;
+    char buffer[MAXLEN] = {0};
+    int ret,rew;
+
+    fs = open(src,O_RDONLY);
+    if(-1 == fs){
+        perror("error");
+        printf("in %d\n",__LINE__);
+        return fs;
+    }
+
+    fd = open(dst,O_WRONLY|O_CREAT|O_TRUNC,0666);
+    if(-1 == fd){
+        perror("error");
+        printf("in %d\n",__LINE__);
+        return fd;
+    }
+
+
+    while(1){
+        ret = read(fs,buffer,MAXLEN);
+        if(ret<MAXLEN){
+            break;
+        }
+        rew = write(fd,buffer,strlen(buffer));
+        if(-1 == rew){
+            perror("error");
+            printf("in %d\n",__LINE__);
+            return rew;
+        }
+    }
+
+    if(ret >0) {
+        buffer[ret] = 0;
+        rew = write(fd,buffer,strlen(buffer));
+        if(-1 == rew){
+            perror("error");
+            printf("in %d\n",__LINE__);
+            return rew;
+        }   
+    }
+
+    close(fs);
+    close(fd);
+    
+    return 0;
+}
+
+int main(){
+    int ret;
+    ret = mycp("b.txt","a.txt");
+
+    if(-1 == ret){
+        printf("copy failed!\n");  
+    } else {
+        printf("copy success!\n");
+    }
+    return 0;
+}
+
+
