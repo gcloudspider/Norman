a251abff81a5c58002df7afe78c7fcece3eb3248
evely211 <569197531@qq.com>
2015-12-09 20:53:30 +0800

    add system ls

---
+++ c/c_system/filesystem/sysls.c
@@ -0,0 +1,75 @@
+/*########################################################
+##File Name: syschmod.c
+##Created Time:2015年12月09日 星期三 09时08分24秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "head.h"
+
+void usage(){
+    fprintf(stderr,"Usage: sysls\n");
+    exit(2);
+}
+
+int filter(const struct dirent* d){
+    if(strcmp(d->d_name,".")==0 || strcmp(d->d_name,"..")==0)
+        return 0;
+    return 1;
+}
+
+
+int main(int argc,char* argv[]){
+    int ch;
+    int ret;
+    DIR *fd;
+    struct dirent *d,**m;
+    struct stat *finfo;
+    int i,count;
+
+    if(argc == 1){
+        //fd = opendir("./");
+        //while(d=readdir(fd)){
+        //    printf("%s\t",d->d_name);   
+        //}
+        count = scandir("./",&m,filter,alphasort);
+        for(i=0;i<count;i++){
+            printf("%s\n",m[i]->d_name);
+        }
+    }
+
+    fd = opendir(argv[1]);
+    while(d=readdir(fd)){
+        stat(d->d_name,finfo);
+        printf("%u",(unsigned int)finfo->st_mode);
+        printf("%s\n",d->d_name);
+    }
+
+
+    while((ch=getopt(argc,argv,"hvl"))!=EOF){
+        switch(ch){
+            case 'h':
+                usage();
+                break;
+            case 'v':
+                break;
+            case 'l':
+                break;
+            default:
+                
+                break;
+        }
+    }
+
+
+}
