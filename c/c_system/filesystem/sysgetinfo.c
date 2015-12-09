0dd50910093dec17bce0441f7f167390bbb2a8ef
evely211 <569197531@qq.com>
2015-12-09 21:11:17 +0800

    add system get file info

--- c/c_system/filesystem/sysgetinfo.c
+++ c/c_system/filesystem/sysgetinfo.c
@@ -19,14 +19,12 @@
 
 
 void usage(){
-    fprintf(stderr,"Usage: sysgetinfo <filename> [-s size] [-b hblock] [-i I/O block] [-l hardlink] [-p pro] [-u uid] [-g git] [-m modifytime] [-a accesstime] [-c chmodtime]\n");
+    fprintf(stderr,"Usage: sysgetinfo <filename> [-s size] [-b hblock] [-i I/O block] [-l hardlink] [-p pro] [-u uid] [-g gid] [-m modifytime] [-a accesstime] [-c chmodtime]\n");
     exit(2);
 }
 
-int getfileinfo(const char* str,struct stat *fileinfo){
-    
+int getfileinfo(const char* str,struct stat *fileinfo){ 
     stat(str,fileinfo);
-
 }
 
 int main(int argc,char* argv[]){
@@ -45,26 +43,34 @@ int main(int argc,char* argv[]){
                 printf("Access Time: %u\n",(unsigned int)finfo.st_atime);
                 break;
             case 'b':
+                printf("hardlink:%u\n",(unsigned int)finfo.st_nlink);
                 break;
             case 'c':
+                printf("laster modify time:%ld\n",finfo.st_ctime);
                 break;
             case 'g':
+                printf("GUID:%u\n",(unsigned int)finfo.st_gid);
                 break;
             case 'h':
                 usage();
                 break;
             case 'i':
+                printf("I/O block:%u\n",(unsigned int)finfo.st_blksize);
                 break;
             case 'l':
+                printf("link:%u\n",finfo.st_nlink);
                 break;
             case 'm':
+                printf("Modify time:%ld\n",finfo.st_mtime);
                 break;
             case 'p':
+                printf("file mode:%u\n",finfo.st_mode);
                 break;
             case 's':
                 printf("Size:%u\n",(unsigned int)finfo.st_size);
                 break;
             case 'u':
+                printf("UID:%u\n",finfo.st_uid);
                 break;
             case 'v':
                 break;
