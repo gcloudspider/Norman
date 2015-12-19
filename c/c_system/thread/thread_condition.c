6f47be200e17c93de7b9f483ddca57e40d971cc1
evely211 <569197531@qq.com>
2015-12-19 09:08:43 +0800

    add thread condition

--- c/c_system/thread/thread_condition.c
+++ c/c_system/thread/thread_condition.c
@@ -15,4 +15,62 @@
 ##########################################################*/
 
 #include<stdio.h>
+#include "head.h"
 
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+
+int num =0;
+
+void* student(void *v){
+    int time;
+    time = rand()%10+1;
+    printf("student %d get running...time=%d\n",(int)v,time);
+    sleep(time);
+    pthread_mutex_lock(&mutex);
+    num++;
+    printf("student %d finish! num=%d\n",(int)v,num);
+    if(num%10 == 0){
+        pthread_cond_signal(&cond);     //触发条件，
+    }
+    pthread_mutex_unlock(&mutex);
+    return v;
+}
+
+void* teacher(void* v){
+    int k = 0;
+    int n=0;
+    while(1){
+        pthread_cond_wait(&cond,&mutex);  //醒来
+        printf("drink beer!num=%d k=%d\n",num,++k);
+
+        if(num==30)break;
+    }
+    printf("teacher finish!!!\n");
+    return v;
+}
+
+int main(){
+    pthread_t tid[30],t;
+    int i;
+    void *ret;
+
+    srand((int)time(0));
+    printf("hello,condition variable test\n");
+    pthread_mutex_init(&mutex,NULL);
+    pthread_cond_init(&cond,NULL);      //条件变量初始化
+
+    pthread_create(&t,NULL,teacher,NULL);
+    sleep(1);
+
+    for(i=0;i<30;i++){
+        pthread_create(&tid[i],NULL,student,(void*)i);
+    }
+
+    for(i=0;i<30;i++){
+        pthread_detach(tid[i]);
+    }
+    pthread_join(t,&ret);
+
+    pthread_exit(0);
+}
