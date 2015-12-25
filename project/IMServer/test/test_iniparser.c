82aea065e9966db015e0533637bb1d83b821c8e5
evely211 <569197531@qq.com>
2015-12-25 09:41:34 +0800

    add ini parser module

---
+++ project/IMServer/test/test_iniparser.c
@@ -0,0 +1,36 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+#include <unistd.h>
+#include "../include/iniparser.h"
+
+int main(){
+    int status;
+    int tmp;
+    dictionary * ini;
+    ini = iniparser_load("switch.ini");
+    if (ini==NULL){
+        fprintf(stderr, "cannot parse file: %s\n", "switch.ini");
+        return -1;
+    }
+    iniparser_dump(ini, stderr);
+
+    /* Get attributes */
+    printf("Mirror:\n");
+    tmp = iniparser_getint(ini, "mirror:mport", -1);
+    printf("mport:         [%x]\n", tmp);
+
+    printf("igmp:\n");
+    tmp = iniparser_getint(ini, "igmp:mport", -1);
+    printf("mport:         [%x]\n", tmp);
+
+    printf("flooding:\n");
+    tmp = iniparser_getint(ini, "flooding:multicast", -1);
+    printf("multicast:     [%x]\n", tmp);
+
+    printf("flooding:\n");
+    tmp = iniparser_getint(ini, "flooding:unicast", -1);
+    printf("unicast:       [%x]\n", tmp);
+    iniparser_freedict(ini);
+    return 0;
+} 
