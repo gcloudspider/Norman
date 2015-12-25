6f06fc7fb2a038444ade5d32a6b4e513a75be824
evely211 <569197531@qq.com>
2015-12-25 10:23:38 +0800

    modify test ini parser path

--- project/IMServer/test/test_iniparser.c
+++ project/IMServer/test/test_iniparser.c
@@ -8,7 +8,7 @@ int main(){
     int status;
     int tmp;
     dictionary * ini;
-    ini = iniparser_load("switch.ini");
+    ini = iniparser_load("./resources/switch.ini");
     if (ini==NULL){
         fprintf(stderr, "cannot parse file: %s\n", "switch.ini");
         return -1;
