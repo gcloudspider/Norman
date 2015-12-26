41e54e376bcd01545aef9c59cb6aa8356ce48181
evely211 <569197531@qq.com>
2015-12-26 11:55:13 +0800

    add include file dir

---
+++ c/c_system/socket/chatroom/include/chatserver.h
@@ -0,0 +1,76 @@
+/*########################################################
+##File Name: head.h
+##Created Time:2015年12月09日 星期三 12时18分25秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#ifndef _CHATSERVER_H
+#define _CHATSERVER_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<sys/stat.h>
+#include<getopt.h>
+#include<sys/types.h>
+#include<sys/syscall.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/mman.h>
+#include<sys/ioctl.h>
+#include<dirent.h>
+#include<sys/time.h>
+#include<signal.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+#include<sys/msg.h>
+#include<sys/shm.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<sys/epoll.h>
+
+/*const*/
+#define DEFAULT_SERVER      "127.0.0.1"
+#define DEFAULT_PORT        8080
+#define DEFAULT_CONNUM      100
+#define DEFAULT_THREADNUM   100
+
+#define DEFAULT_CFGPATH     "../conf/chatroom.cfg"
+/*struct*/
+
+typedef struct servercfg{
+    const char* server_ip;
+    int  server_port;
+    int  server_connum;
+    int  server_threadnum;
+    const char* dbpath;
+}SEVCF;
+
+typedef struct userinfo{
+    int fd;
+    int online;
+    char username[64];
+    char passwd[128];
+    struct userinfo *pre;
+    struct userinfo *next;
+}USERINFO;
+
+/*function*/
+int init_conf(SEVCF* cf,const char* cpath);
+
+int init_db(USERINFO* uinfo,USERINFO** uhead,const char* dbpath);
+
+#endif
