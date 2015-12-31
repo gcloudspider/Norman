69bc8242af8614a79e850a4db0eaa0071611e406
evely211 <569197531@qq.com>
2015-12-31 16:59:10 +0800

    modify sql script

--- project/didi/db/didi.sql
+++ project/didi/db/didi.sql
@@ -11,29 +11,37 @@ MySQL - 5.6.12-log : Database - mailarchive
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`dadabus` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`didi` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `dadabus`;
+USE `didi`;
 
 /*Table structure for table `dadauser` */
 
-DROP TABLE IF EXISTS `dadauser`;
+DROP TABLE IF EXISTS `didiuser`;
 
-CREATE TABLE `dadauser` (
+CREATE TABLE `didiuser` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `userid` int(10) NOT NULL COMMENT '用户ID',
   `username` varchar(255) NOT NULL COMMENT '用户名',
   `nickname` varchar(255) NOT NULL COMMENT '昵称',
   `passwd` varchar(255) NOT NULL COMMENT '密码',
   `telphone` varchar(255) NOT NULL COMMENT '电话号码',
-  `orders` varchar(255) NOT NULL COMMENT '订单号',
   PRIMARY KEY (`id`),
   UNIQUE KEY `userid` (`userid`),
   KEY `name` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4959 DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `dadaorders`;
+DROP TABLE IF EXISTS `didiorders`;
+
+CREATE TABLE `didiorders` (
+    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `orderid` int(10) NOT NULL COMMENT '订单号',
+    `starting` varchar(255) NOT NULL COMMENT '出发地',
+    `destination` varchar(255) NOT NULL COMMENT '目的地',
+    `userid` int(10) NOT NULL COMMENT '订单发起人id',
+    `driverid` int(10) NOT NULL COMMENT '订单接收司机id',
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
