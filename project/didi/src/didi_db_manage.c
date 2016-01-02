2defb6eb38a06166d29c99731cc4bcd2730838f2
evely211 <569197531@qq.com>
2016-01-03 00:39:06 +0800

    rename database to db

---
+++ project/didi/src/didi_db_manage.c
@@ -0,0 +1,38 @@
+/*########################################################
+##File Name: didi_database_manage.c
+##Created Time:2015年12月29日 星期二 20时53分42秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "../include/didi.h"
+
+int didi_db_init(MYSQL *db,DIDISQL didimysql,zlog_category_t **c){
+    char *sql;
+    int res;
+    mysql_init(db);
+    if(mysql_real_connect(db,didimysql.hostname,didimysql.username,didimysql.passwd,didimysql.dbname,0,NULL,0)){
+        zlog_info(*c,"didi connection db success!\n");
+        mysql_query(db,"SET NAMES UTF8");
+    } else {
+        zlog_info(*c,"didi connect db failed!\n");
+        if(mysql_error(db)){
+            zlog_info(*c,"connect error %d:%s\n",mysql_errno(db),mysql_error(db));
+        }
+    }
+}
+
+int didi_db_release(MYSQL *db,zlog_category_t **c){
+    mysql_close(db);
+    zlog_info(*c,"didi release db success!\n");
+}
