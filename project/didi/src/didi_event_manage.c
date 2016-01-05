7d09410eecd8f75415adae301ab518e3488e0c38
evely211 <569197531@qq.com>
2016-01-05 15:13:10 +0800

    add event handle func

---
+++ project/didi/src/didi_event_manage.c
@@ -0,0 +1,34 @@
+/*########################################################
+##File Name: didi_event_manage.c
+##Created Time:2016年01月05日 星期二 14时26分49秒
+##Author: Norman 
+##Description: 
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+#include "../include/didi.h"
+
+void* didi_event_register(void *argv){
+
+}
+
+void* didi_event_login(void *argv){
+
+}
+
+void* didi_event_logout(void *argv){
+
+}
+
+void* didi_event_query(void *argv){
+
+}
