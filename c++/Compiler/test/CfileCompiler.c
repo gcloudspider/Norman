c042227b70a1bda63d2b4eb9b693408689363207
evely211 <569197531@qq.com>
2016-01-13 08:51:28 +0800

    add Compiler Document

---
+++ c++/Compiler/test/CfileCompiler.c
@@ -0,0 +1,23 @@
+/*########################################################
+##File Name: CfileCompiler.c
+##Created Time:2016年01月13日 星期三 08时48分58秒
+##Author: Norman 
+##Description: 示例程序:更简便的记忆方法
+##
+##Issue: 
+##
+##Analyze: 
+##
+##Induction:
+##
+##Summary:
+##
+##########################################################*/
+
+#include<stdio.h>
+
+int main(){
+
+/*未初始化的全局变量的定义*/
+int x_global_uninit;
+}
