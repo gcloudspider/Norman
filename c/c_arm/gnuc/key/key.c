1ba253748e44b2b47df1776e5f1a0827231682a1
evely211 <569197531@qq.com>
2016-01-19 20:17:02 +0800

    add keyboard

---
+++ c/c_arm/gnuc/key/key.c
@@ -0,0 +1,52 @@
+/**
+*  - version 1.0
+* -------------------------------
+* Copyright (C) 2016-2016 by Norman (none_lih@163.com) 
+* Report bugs and download new versions at https//github.com/evely211
+* 
+* This library is distributed under the MIT License. See notice at the end of this file.
+* This work is based on POSIX，which is:
+* Copyright (C) 2016,by Norman
+*/
+#define rGPGCON \
+        *(volatile unsigned*) 0x56000060
+#define rGPGDAT \
+        *(volatile unsigned*) 0x56000064
+
+#define GPG0_IN         ~(3<<0*2)
+#define GPG3_IN         ~(3<<3*2)
+#define GPG5_IN         ~(3<<5*2)
+#define GPG6_IN         ~(3<<6*2)
+#define GPG7_IN         ~(3<<7*2)
+#define GPG11_IN        ~(3<<11*2)
+
+void init_key(){
+    rGPGCON &= GPG0_IN&GPG3_IN&GPG5_IN
+        &GPG6_IN&GPG7_IN&GPG11_IN;
+    //0000 0000 0000 0000
+}
+
+int isKey(int num){
+    switch(num){
+        case 1:
+            return rGPGDAT&(1<<0)?0:1;
+        case 2:
+            return rGPGDAT&(1<<3)?0:1;
+        case 3:
+            return rGPGDAT&(1<<5)?0:1;
+        case 4:
+            return rGPGDAT&(1<<6)?0:1;
+        case 5:
+            return rGPGDAT&(1<<7)?0:1;
+        case 6:
+            return rGPGDAT&(1<<11)?0:1;
+        default:
+            return 0;
+    }
+}
+
+int key_press(){
+    int i;
+    for(i=1;i<=6;i++)
+        if(isKey(i)) return i;
+}
