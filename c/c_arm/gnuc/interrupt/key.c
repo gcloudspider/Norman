eb33fd5095baa158d0eea1fc23d980e85e927d7a
evely211 <569197531@qq.com>
2016-01-20 16:57:06 +0800

    add initerrupt

---
+++ c/c_arm/gnuc/interrupt/key.c
@@ -0,0 +1,48 @@
+/**
+*  - version 1.0
+* -------------------------------
+* Copyright (C) 2016-2016 by Norman (none_lih@163.com) 
+* Report bugs and download new versions at https//github.com/evely211
+* 
+* This library is distributed under the MIT License. See notice at the end of this file.
+* This work is based on POSIX，which is:
+* Copyright (C) 2016,by Norman
+*/
+#include "include/gpio.h"
+
+void init_key(){
+    rGPGCON &= GPG0_IN&GPG3_IN&GPG5_IN
+        &GPG6_IN&GPG7_IN&GPG11_IN;
+    //初始化为输入模式
+    //1100 0000 1111 1100 1100 0011
+}
+
+int isKey(int num){
+    //默认不按按钮是高电平rGPGDAT值为1
+    //按下按钮为0
+    switch(num){
+        case 1:
+            //0与1为0 否返回1
+            return rGPGDAT&(1<<0)?0:1;
+        case 2:
+            return rGPGDAT&(1<<3)?0:1;
+        case 3:
+            return rGPGDAT&(1<<5)?0:1;
+        case 4:
+            return rGPGDAT&(1<<6)?0:1;
+        case 5:
+            return rGPGDAT&(1<<7)?0:1;
+        case 6:
+            return rGPGDAT&(1<<11)?0:1;
+        default:
+            return 0;
+    }
+}
+
+int key_press(){
+    int i;
+    for(i=1;i<=6;i++){
+        //判断按钮是否按下
+        if(isKey(i)) return i;
+    }   
+}
---
+++ c/c_arm/gnuc/interrupt/main.c
@@ -0,0 +1,36 @@
+#include "include/gpio.h"
+
+void delay(int n)
+{
+	while(n--);
+}
+
+int main()
+{
+	init_led();
+    init_beep();
+    //init_key();  轮循初始化
+	int i;
+	while(1){
+        //while 循环轮循方式 (另一种方法中断方式)
+        /*
+        i=key_press();
+        if(i>=1 && i<=4){
+            led_off_all();
+            beep_off();
+            
+            led_on(i);
+            beep_on();
+        } else if(i == 5){
+            led_off_all();
+            beep_off();
+            
+            led_on_all();
+            beep_on();
+        }else {
+            led_off_all();
+            beep_off();
+        }*/
+	}
+	return 0;
+}
