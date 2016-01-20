cf5b5be042550fb671b05bcf665cc9def36bf795
evely211 <569197531@qq.com>
2016-01-20 10:57:12 +0800

    modify key main

--- c/c_arm/gnuc/key/main.c
+++ c/c_arm/gnuc/key/main.c
@@ -16,9 +16,15 @@ int main()
         //while 循环轮循方式 (另一种方法中断方式)
         i=key_press();
         if(i>=1 && i<=4){
+            led_off_all();
+            beep_off();
+            
             led_on(i);
             beep_on();
         } else if(i == 5){
+            led_off_all();
+            beep_off();
+            
             led_on_all();
             beep_on();
         }else {
