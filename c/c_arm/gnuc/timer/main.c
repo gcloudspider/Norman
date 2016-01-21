95bc83bee0cf1eb8f9bf9e1813d37aa4a662ed40
evely211 <569197531@qq.com>
2016-01-21 20:46:16 +0800

    sloved timer issuse

---
+++ c/c_arm/gnuc/timer/main.c
@@ -0,0 +1,23 @@
+#include "include/gpio.h"
+
+void delay(int n)
+{
+	while(n--);
+}
+
+int main()
+{
+    init_clock();
+	init_led();
+    init_beep();
+    //init_key();  轮循初始化
+    //init_interrupt_for_key();   //中断的方式
+
+    init_timer4();
+    init_interrupt_for_timer4();
+    led_off_all();
+	int i;
+	while(1){
+	}
+	return 0;
+}
---
+++ c/c_arm/gnuc/timer/timer.c
@@ -0,0 +1,32 @@
+/**
+*  - version 1.0
+* -------------------------------
+* Copyright (C) 2016-2016 by Norman (none_lih@163.com) 
+* Report bugs and download new versions at https//github.com/evely211
+* 
+* This library is distributed under the MIT License. See notice at the end of this file.
+* This work is based on POSIX，which is:
+* Copyright (C) 2016,by Norman
+*/
+#include "include/gpio.h"
+
+void init_timer4(){
+    /*
+    *公式:
+    *TF = PCLK /{prescaler value+1} /{divider value};
+    *{prescaler value} = 0~255
+    *{divider value} = 2,4,8,16
+    * */
+    rTCFG0 = 99<<8;  //prescaler:99+1=100
+    rTCFG1 = 3<<16;  //divider 16
+    rTCNTB4 = 31250/2;  //设置时间 0.5秒
+
+    rTCON |= 1<<21;  //设置手动更新计时器4
+    rTCON = 5<<20;
+    //5  = 0101 左移20位
+    //将[22] [21] [20] 第20 22位置1
+    //开启计时器
+    //开启自动重置数
+}
+
+
