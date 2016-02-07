e18a3cd53293c9b83ddeec39ad188a3bc12c2a95
evely211 <569197531@qq.com>
2016-02-07 23:49:20 +0800

    modify visitor pattern base example

--- books/Design_Pattern/Visitor/Base/BaseVisitor.cpp
+++ books/Design_Pattern/Visitor/Base/BaseVisitor.cpp
@@ -34,5 +34,43 @@ string CBaseVisitor::GetBasicInfo(CEmployee *pemployee){
     info.append("姓名:");
     info.append(pemployee->GetName());
     info.append("\t");
+    info.append("性别:");
+    info.append(CConvert::ToString(pemployee->GetSex()));
+    info.append("\t");
+    inco.append("薪水:");
+    info.append(CConvert::ToString(pemployee->GetSalary()));
+    info.append("\t");
+    return info;
+}
 
+string CBaseVisitor::GetManagerInfo(CManager manager){
+    string basicInfo = this->GetBasicInfo(&manager);
+    string otherInfo = "";
+    otherInfo.append("业绩");
+    otherInfo.append(manager.GetPerformance());
+    otherInfo.append("\t");
+    basicInfo.append(otherInfo);
+    return basicInfo;
+}
+
+string CBaseVisitor::GetCommonEmployee(CCommonEmployee employee){
+    string basicInfo = this->GetBasicInfo(&employee);
+    string otherInfo = "";
+    otherInfo.append("工作:");
+    otherInfo.append(employee.GetJob());
+    otherInfo.append("\t");
+    basicInfo.append(otherInfo);
+    return basicInfo;
+}
+
+int CBaseVisitor::GetTotalSalary(){
+    return this->m_commonTotal+this->m_managerTotal;
+}
+
+void CBaseVisitor::CalCommonSalary(int salary){
+    this->m_commonTotal += salary;
+}
+
+void CBaseVisito::CalManagerSalary(int salary){
+    this->m_managerTotal += salary;
 }
