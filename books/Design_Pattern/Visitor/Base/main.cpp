ee662fd7bf2c44ef9b02a3705eb1680f0ee7468d
evely211 <569197531@qq.com>
2016-02-08 21:32:03 +0800

    add main

--- books/Design_Pattern/Visitor/Base/main.cpp
+++ books/Design_Pattern/Visitor/Base/main.cpp
@@ -18,4 +18,44 @@ void MockEmployee(vector<CEmployee*> *pvce){
     pZhangSan->SetJob("编写Java程序,绝对蓝领,苦工加搬运工");
     pZhangSan->SetName("张三");
     pZhangSan->SetSalary(1800);
+    pZhangSan->SetSex(CEmployee::MALE);
+    pvce->push_back(pZhangSan);
+
+    CCommonEmployee *pLiSi = new CCommonEmployee();
+    pLiSi->SetJob("页面美工,审美素质太不流行了!");
+    pLiSi->SetName("李四");
+    pLiSi->SetSalary(1900);
+    pLiSi->SetSex(CEmployee::FEMALE);
+    pvce->push_back(pLiSi);
+
+    CManager *pWangWu = new CManager();
+    pWangWu->SetPerformance("基本上是负值,但是我会拍马屁呀!");
+    pWangWu->SetName("王五");
+    pWangWu->SetSalary(1900);
+    pWangWu->SetSex(CEmployee::FEMALE);
+    pvce->push_back(pWangWu);
+}
+
+void DoIt(){
+    vector<CEmployee*> vce;
+    MockEmployee(&vce);
+    vector<CEmployee*>::const_iterator readIt = vec.begin();
+
+    CBaseVisitor visitor;
+    for(;readIt !=vce.end();readIt++){
+        (*readIt)->Accept(&visitor);
+    }
+    cout<<"本公司的月工资总额是:"<<CConvert::ToString(visitor.GetTotalSalary()).c_str()<<endl;
+
+    vector<CEmploy*>::reverse_iterator delIt = vce.rbegin();
+    for(; delIt != vce.rend();delIt++){
+        delete (*delIt);
+        (*delIt) = NULL;
+    }
+    vce.clear();
+}
+
+int main(){
+    DoIt();
+    return 0;
 }
