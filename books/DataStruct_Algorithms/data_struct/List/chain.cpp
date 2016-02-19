5d999e2ef1a3a2151308b84fc5f12ec17220fe24
evely211 <569197531@qq.com>
2016-02-19 09:43:43 +0800

    modify data struct List

--- books/DataStruct_Algorithms/data_struct/List/chain.cpp
+++ books/DataStruct_Algorithms/data_struct/List/chain.cpp
@@ -39,3 +39,96 @@ chain<T>::chain(const chain<T>& theList){
     }
     targetNode->next = NULL;
 }
+
+template<class T>
+chain<T>::~chain(){
+    while(firstNode != NULL){
+        chainNode<T>* nextNode = firstNode->next;
+        delete firstNode;
+        firstNode = nextNode;
+    }
+}
+
+template<class T>
+T& chain<T>::get(int theIndex) const{
+    checkIndex(theIndex);
+
+    chainNode<T>* currentNode = firstNode;
+    for(int i=0;i<theIndex;i++){
+        currentNode = currentNode->next;
+    }
+    return currentNode->element;
+}
+
+template<class T>
+int chain<T>::indexOf(const T& theElement) const{
+    chainNode<T>* currentNode = firstNode;
+    int index = 0;
+    while(currentNode != NULL && currentNode->element != theElement){
+        currentNode = currentNode->next;
+        index++;
+    }
+
+    if(currentNode == NULL){
+        return -1;
+    } else {
+        return index;
+    }
+}
+
+template<class T>
+void chain<T>::erase(int theIndex){
+    checkIndex(theIndex);
+    chainNode<T>* deleteNode;
+
+    if(theIndex == 0){
+        deleteNode = firstNode;
+        firstNode = firstNode->next;
+    } else {
+        chainNode<T>* p = firstNode;
+        for(int i=0;i< theIndex -1;i++){
+            p = p->next;
+        }
+        deleteNode = p->next;
+        p->next = p->next->next;
+    }
+    listSize --;
+    delete deleteNode;
+}
+
+//插入
+template<class T>
+void chain<T>::insert(int theIndex,const T& theElement){
+    if(theIndex < 0|| theIndex > listSize){
+        ostringstream s;
+        s <<"index = "<<theIndex << "size = "<<listSize;
+        throw illegalIndex(s.str());
+    }
+    //插入到头
+    if(theIndex == 0){
+        firstNode = new chainNode<T>(theElement,firstNode);
+    }else{
+        chainNode<T>* p = firstNode;
+        for(int i =0;i<theIndex -1; i++){
+            p = p->next;
+        }
+        p->next = new chainNode<T>(theElement,p->next);
+    }
+    listSize++;
+}
+
+template<class T>
+void chain<T>::output(ostream& out) const{
+    for(chainNode<T>* currentNode = firstNode;
+       currentNode != NULL;
+       currentNode = currentNode->next)
+    
+    out<<currentNode->element << " ";
+}
+
+//重载<<
+template<class T>
+ostream& operator<<(ostream& out,const chain<T>& x){
+    x.output(out);
+    return out;
+}
