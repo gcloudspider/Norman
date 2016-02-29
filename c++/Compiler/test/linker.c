5aa665e60e32f0dce4cb5199bf033b6f1a8faa24
evely211 <569197531@qq.com>
2016-02-29 19:35:34 +0800

    add compiler

--- c++/Compiler/test/linker.c
+++ c++/Compiler/test/linker.c
@@ -1,32 +1,32 @@
--/*########################################################
--####File Name: linker.c
--####Created Time:2016年01月14日 星期四 08时48分22秒
--####Author: Norman 
--####Description: 
--####
--####Issue: 
--####
--####Analyze: 
--####
--####Induction:
--####
--####Summary:
--####
--##########################################################*/
--
--#include<stdio.h>
--
--int z_global = 11;
--
--static int y_global_init = 2;
--
--extern int x_global_init;
--
--int fn_a(int x,int y){
--    return (x+y);
--}
--
--int main(int argc,char* argv[]){
--    const char* message = "Hello,world";
--    return fn_a(11,12);
--}
+/*########################################################
+####File Name: linker.c
+####Created Time:2016年01月14日 星期四 08时48分22秒
+####Author: Norman 
+####Description: 
+####
+####Issue: 
+####
+####Analyze: 
+####
+####Induction:
+####
+####Summary:
+####
+##########################################################*/
+
+#include<stdio.h>
+
+int z_global = 11;
+
+static int y_global_init = 2;
+
+extern int x_global_init;
+
+int fn_a(int x,int y){
+    return (x+y);
+}
+
+int main(int argc,char* argv[]){
+    const char* message = "Hello,world";
+    return fn_a(11,12);
+}
