491153d3a8b22ee7a1aa1afa4042212c01b139e2
evely211 <569197531@qq.com>
2016-03-02 09:52:08 +0800

    cleanup code

--- c++/Base/basis/decltype.cpp
+++ c++/Base/basis/decltype.cpp
@@ -16,6 +16,15 @@ using namespace std;
 //C++11 不支持自动推导返回类型
 //C++11 推荐使用decltype解决返回类型问题
 /*
+####decltype关键字新特性:
+    decltype与auto区别:
+        1.auto修饰的变量必须初始化,编译器通过初始化确定auto代表类型
+        2.编译时推导出一个表达式的类型
+
+####C++14与C++11 decltype关键字:
+    C++11 新增decltype(a+b)
+    C++14 取消了decltype
+
 auto add(int a,int b)->decltype(a+b){
     int i = a+b;
     return i;
--- c++/Base/basis/template.cpp
+++ c++/Base/basis/template.cpp
@@ -8,6 +8,21 @@
 * This work is based on POSIX，which is:
 * Copyright (C) 2016,by Norman
 */
+/*
+####模板改进:
+    1.模板右尖括号:
+        C++98/03中模板实例化连续两个右尖括号会被编译器解释成右移操作符,而不是模板参数表的结束
+        C++11中使用()括号解决:
+            Foo<(100 >> 2)>xx;
+
+    2.模板别名:
+        C++98/03: typedef 重定义一个类型
+        C++11: using别名
+
+    3.函数模板的默认模板参数:
+        C++98/03: 允许类模板可以有默认模板参数/不允许函数的默认模板参数
+        C++11: 可以直接使用函数模板参数
+*/
 #include<iostream>
 using namespace std;
 
