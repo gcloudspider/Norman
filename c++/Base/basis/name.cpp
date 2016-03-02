0fe528fcdf39cdd445f9bf7ecb1432a4676bcabf
evely211 <569197531@qq.com>
2016-03-02 20:40:42 +0800

    add namespace use

--- c++/Base/basis/name.cpp
+++ c++/Base/basis/name.cpp
@@ -1,53 +1,95 @@
-/**
-*  - version 1.0
-* -------------------------------
-* Copyright (C) 2016-2016 by Norman (none_lih@163.com) 
-* Report bugs and download new versions at https//github.com/evely211
-* 
-* This library is distributed under the MIT License. See notice at the end of this file.
-* This work is based on POSIX，which is:
-* Copyright (C) 2016,by Norman
-*/
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <iostream>
-using namespace std;
 
-struct Person{
-    char username[10];
-    int age;
+struct Person {
+	char name[20];
+	int old;
 };
 
-
-namespace shenzhen{
-    Person person;
+namespace ShangHai {
+	Person person;
+}
+namespace ShenZhen {
+	Person person;
 }
 
-namespace guangzhou{
-    Person person;
+
+
+void selectType()
+{
+	printf("   1.输入\n");
+	printf("   2.输出\n");
+	printf("   3.退出\n");
+	printf("请选择: ");
+}
+void selectArea()
+{
+	system("clear");
+	printf("   1.上海\n");
+	printf("   2.深圳\n");
+	printf("请选择: ");
 }
 
-namespace beijin{
-    Person person;
+void input(int cmd)
+{
+	Person temp;
+	switch (cmd) {
+	case 1:
+		printf("姓名 : ");
+		scanf("%s", temp.name);
+		printf("年龄 : ");
+		scanf("%d", &temp.old);
+		ShangHai::person = temp;
+		break;
+	case 2:
+		printf("姓名 : ");
+		scanf("%s", temp.name);
+		printf("年龄 : ");
+		scanf("%d", &temp.old);
+		ShenZhen::person = temp;
+		break;
+	}
+}
+void output(int cmd)
+{
+	switch (cmd) {
+	case 1:
+		printf("姓名 : %s\n", ShangHai::person.name);
+		printf("年龄 : %d\n", ShangHai::person.old);
+		break;
+	case 2:
+		printf("姓名 : %s\n", ShenZhen::person.name);
+		printf("年龄 : %d\n", ShenZhen::person.old);
+		break;
+	}
 }
 
-void usage(){
-    cout<<"name: <username> <age> <city>"<<endl;
-    exit(1);
-}
-
-auto print_result(int num){
-    switch(num){
-        case 1:
-            break;
-        case 2:
-            break;
-    }
-}
-
-int main(int argc,char* argv[]){
-
-    auto num =0;
-
-    return 0;
+int main()
+{
+	int cmd = 0;
+	system("clear");
+label:
+	selectType();
+	scanf("%d", &cmd);
+	switch (cmd) {
+	case 1:
+		selectArea();
+		scanf("%d", &cmd);
+		input(cmd);
+		goto label;
+		break;
+	case 2:
+		selectArea();
+		scanf("%d", &cmd);
+		output(cmd);
+		goto label;
+		break;
+	case 3:
+		return 0;
+	default:
+		printf("选择错误，重新选择...\n");
+		goto label;
+		break;
+	}
+	return 1;
 }
