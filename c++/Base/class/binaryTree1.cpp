2cf84cf8448e215586550870dea4101a4d9b65de
evely211 <569197531@qq.com>
2016-03-08 08:58:51 +0800

    modify binaryTree remove node and childer

--- c++/Base/class/binaryTree1.cpp
+++ c++/Base/class/binaryTree1.cpp
@@ -58,10 +58,119 @@ public:
         }
     }
 
-    void RemoveOne(int data) {
+    bool RemoveNodeAndChilder(int data) {
         //找到节点
-        Node* pn = FindParentNode(root,data);
-        if (pn->left != nullptr && pn->right == nullptr)
+        if (root == nullptr) return false;
+        //要删除的节点为根节点
+        if (root->data == data) {
+            Destory(root);
+            root = nullptr;
+            return true;
+        }
+
+        //查找父节点
+        Node* p = FindParentNode(root,data);
+        if (p != nullptr) {
+            //二叉树左边不为空
+            if (p->left != nullptr) {
+                if (p->left->data == data) {
+                    Destory(p->left);
+                    p->left = nullptr;
+                } else {
+                    
+                }
+            } else {
+                Destory(p->right);
+                p->right = nullptr;
+            }
+            return true;
+        } else {
+            return false;       //要删除的节点不存在二叉树中
+        }
+    }
+
+    bool RemoveNode(int data) {
+        int flag = 0;
+
+        if (root == nullptr) return false;
+
+        //判断是否根节点就是要删除的节点
+        if(root->data == data ) {
+            if (root->right == nullptr && root->left == nullptr) {
+                //若没有子树
+                delete root;
+                root = nullptr;
+            } else if ( root->left == nullptr) {  //判断是否有左子树
+                Node* p = root->right;
+                delete root;
+                root = p;
+            } else if (root->right == nullptr) {  //判断是否有右子树
+                Node* p = root->left;
+                delete root;
+                root = p;
+            } else {        //既有左子树,右有右子树
+                Node* p = root->right;
+                Node* t = root->right;
+                while (p->left != nullptr) {
+                    p = p->left;
+                }
+                p->left = root->left;
+                delete root;
+                root = t;
+            }
+            return true;
+        }
+        
+        //要删除的不是根节点
+        Node* p = FindParentNode(root,data);
+
+        if (p == nullptr) {
+            return false;
+        } else {
+            Node* t = nullptr;
+            if (p->data < data) {
+                t = p->right;
+                if (t->left == nullptr && t->right == nullptr ) {
+                    delete t;
+                    p->right = nullptr;
+                } else if (t->left == nullptr) {
+                    p->right = t->right;
+                    delete t;
+                } else if (t->right == nullptr) {
+                    p->right = t->left;
+                    delete t;
+                } else {
+                    Node* r = t->right;
+                    while(r->left != nullptr) {
+                        r = r->left;
+                    }
+                    r->left = t->left;
+                    p->right = t->right;
+                    delete t;
+                }
+            } else if (p->data > data) {
+                t = p->left;
+                if (t->left == nullptr && t->right == nullptr) {
+                    delete t;
+                    p->left = nullptr;
+                } else if (t->left == nullptr) {
+                    p->left = t->right;
+                    delete t;
+                } else if (t->right == nullptr) {
+                    p->left = t->left;
+                    delete t;
+                } else {
+                    Node* r = t->right;
+                    while(r->left != nullptr) {
+                        r = r->left;
+                    }
+                    r->left = t->left;
+                    p->left = t->right;
+                    delete t;
+                }
+            }
+        }
+        return true;
     }
 
     void Destory (Node* p) {
@@ -112,7 +221,7 @@ private:
             parentNode = p;
             return FindParentNode(p->right,data);
         } else if (p->data > data) {
-            parent = p;
+            parentNode = p;
             return FindParentNode(p->left,data);
         } else {
             return parentNode;
@@ -144,9 +253,9 @@ private:
 
     void Print(Node* p) {
         if(p != nullptr) {
-            cout << p->data << " " ;
-            Print(p->left);
-            Print(p->right);
+            cout << p->data << " " ;  //前序遍历
+            Print(p->left);             //中序遍历:先遍右输出左子树，从小到大排序
+            Print(p->right);        //后序遍历:
         }   
     }
 
@@ -160,7 +269,7 @@ int main() {
     t.Show();
     cout << "GetMin= " << t.Max() << endl;
     cout << "GetMin= " << t.Min() << endl;
-    t.Remove(6);
+    t.RemoveNode(8);
     t.Show();
     return 0;
 }
