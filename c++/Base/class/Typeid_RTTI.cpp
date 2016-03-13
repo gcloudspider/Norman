6c9cc2822e54b67dd233899c1efee0de93b4aad5
evely211 <569197531@qq.com>
2016-03-13 20:37:00 +0800

    add Typeid and RTTI

---
+++ c++/Base/class/Typeid_RTTI.cpp
@@ -0,0 +1,35 @@
+/**
+* Create Date:2016年03月13日星期日 08:09:48
+* -------------------------------
+* Copyright (C) 2016-2016 by Norman (none_lih@163.com) 
+* Report bugs and download new versions at https//github.com/evely211
+* 
+* This library is distributed under the MIT License. See notice at the end of this file.
+* This work is based on POSIX，which is:
+* Copyright (C) 2016,by Norman
+*/
+
+#include<iostream>
+using namespace std;
+
+//typeid运算符后接一个类型名或一个表达式,该运算符返回一个类型为std::typeinf的对象的const引用
+
+//type_info是std中的一个类,它用于记录与类型相关的信息
+/*
+class type_info {
+public:
+    virtual ~type_info();
+    bool operator==(const type_info&)const;
+    bool operator!=(const type_info&)const;
+    bool before(const type_info&)const;
+    const char* name()const;
+private:
+    type_info(const type_info&);
+    type_info& operator=(const type_info&);
+};
+*/
+
+int main() {
+
+    return 0;
+}
