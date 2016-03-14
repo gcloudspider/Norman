696ca2aee418658886770620bc17de94c684ca28
evely211 <569197531@qq.com>
2016-03-15 07:18:30 +0800

    add stl use

--- c++/Base/class/STL_map.cpp
+++ c++/Base/class/STL_map.cpp
@@ -12,7 +12,62 @@
 #include<iostream>
 using namespace std;
 #include<map>
+/*
+template <typename T, typename B>
+struct Pair {
+    Pair(){}
+    Pair(T f,B s):first(f),second(s){}
+    Pair(const Pairt<T>& data) {}
 
+    T first;
+    B second;
+};
+*/
+//1.自动排序(按key 字典序排序)
+//2.插入key元素相同时忽略插入
+//
 int main() {
-    pair<string,string> a;
+    pair<string,string> a("norman","产品汪");
+
+    map<string,string> addr;
+    addr.insert(a);
+
+    addr.insert(pair<string,string>("Danny","射鸡师"));
+    addr.insert({"Tonny","程序猿"});
+    addr.insert({"Tonny","程序媛"});
+    addr.erase("Tonny");
+
+    string v = addr["Danny"];           //引用返回
+    addr["Danny"] = "码农";
+    addr["evely211"] = "搬砖的";
+    addr["cell390"] = "架构师";
+    cout << v << endl;
+
+    for (auto it = addr.begin(); it != addr.end();++it) {
+        cout << it->first << ":" << it->second << endl;
+    }
+
+    map<string,string>::iterator it = addr.find("norman");
+    if(it != addr.end()) {
+        cout << "find : " << it->first << " : " << it->second << endl;
+    }
+    
+    string tmpString;
+    int i=1;
+    map<char,int> num;
+    cin >> tmpString;
+    for (auto& tmp : tmpString) {
+        auto it = num.find(tmp);
+        if(it != num.end()) {
+            int j = it->second;
+            num[tmp] = ++j;
+            cout << j << endl;
+            cout << num[tmp] << endl;
+        }
+        num[tmp] = i;
+    }
+
+    for (auto it = num.begin();it != num.end();++it) {
+        cout << it->first << ":" << it->second << endl;
+    }
 }
