6cac74fa53ba24c157699b6931e971e0e622ef62
evely211 <569197531@qq.com>
2016-03-18 13:17:57 +0800

    add search text

---
+++ c++/Base/class/SearchText/include/NotQuery.h
@@ -0,0 +1,14 @@
+/**
+* Create Date:2016年03月18日星期五 13:16:11
+* -------------------------------
+* Copyright (C) 2016-2016 by Norman (none_lih@163.com) 
+* Report bugs and download new versions at https//github.com/evely211
+* 
+* This library is distributed under the MIT License. See notice at the end of this file.
+* This work is based on POSIX，which is:
+* Copyright (C) 2016,by Norman
+*/
+
+#ifndef _NOTQUERY_H
+#define _NOTQUERY_H
+#endif
